f9a58c63e641d9681e74acf245a9320921841cf0
BlivionIaG <kev29lt@gmail.com>
2019-05-12 13:10:09 +0200

    ALL

--- led_pwm/init_IO_ADC_PWM.sh
+++ led_pwm/init_IO_ADC_PWM.sh
@@ -6,12 +6,12 @@ echo am33xx_pwm > slots
 
 echo bone_pwm_P9_14 > slots
 
-echo 1800000000 > /sys/devices/ocp.3/pwm_test_P9_14.15/period
+echo 1800 > /sys/devices/ocp.3/pwm_test_P9_14.15/period
 
 echo 0 > /sys/devices/ocp.3/pwm_test_P9_14.15/polarity
 
 echo 0 > /sys/devices/ocp.3/pwm_test_P9_14.15/run
 
-echo 900000000 > /sys/devices/ocp.3/pwm_test_P9_14.15/duty
+echo 100 > /sys/devices/ocp.3/pwm_test_P9_14.15/duty
 
 echo 1 > /sys/devices/ocp.3/pwm_test_P9_14.15/run
